85bb4fa3bf7a2934503e4287bfc58977d1dc08dd
ranjith <ranjithkankati2@gmail.com>
2026-05-25 07:46:55 +0530

    let example

---
+++ let.js
@@ -0,0 +1,6 @@
+console.log("Before Iniilizaion",yourName);
+let yourName = "ranjith";
+console.log("After initilization",yourName);
+
+
+// Here , let variable throws error if we use before initilization. because it's locked
